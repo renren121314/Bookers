baef00fa4fb5c5d8f5705b36d6ff5d7be501f749
renren121314 <renren199612@gmail.com>
2022-10-16 10:30:55 +0000

    last

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,17 @@
 class BooksController < ApplicationController
-  def new
-    
-    @book = Book.new
-  end
-  
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
     @book = Book.new(book_params)
     if @book.save
-      redirect_to book_path(book.id)
+      flash[:notice] = 'Book was successfully created.'
+      redirect_to book_path(@book.id)
     else
       render :new
-    end    
+    end
   end
 
   def show
@@ -24,22 +21,29 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.save
+      book.update(book_params)
+      flash[:notice] = 'Book was successfully updated.'
+      redirect_to book_path(book.id)
+    else
+      flash[:alert] = 'タイトルや本文を空白にすることはできません'
+      redirect_to edit_book_path(@book.id)
+    end
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to '/books'
   end
-  
+
   private
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
